cc977ab1e7cd9fd7cc44f0edbc985e7309ff0c98
Vincent <tovincentngo@gmail.com>
2025-01-23 10:35:22 +0100

    fichier google layout  page famille et page propos

---
+++ src/components/ui/header.jsx
@@ -0,0 +1,19 @@
+import Link from 'next/link';
+import MainNav from './main-nav';
+import MobileNav from './mobile-nav';
+
+export default function Header() {
+  return (
+    <header className='top-0 w-full border-b'>
+
+        {/* Desktop */}
+        <MainNav />
+
+        {/* Mobile */}
+        <MobileNav />
+
+        
+
+    </header>
+  );
+}
