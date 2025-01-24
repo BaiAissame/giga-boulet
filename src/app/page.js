4fef697d943848f225f40200a516171a7b638381
Bai <Bai_a@outlook.fr>
2025-01-25 00:38:26 +0100

    Ajout Image

--- src/app/page.js
+++ src/app/page.js
@@ -40,17 +40,17 @@ export default function Home() {
                     <h2 className="text-3xl font-bold text-teal-900 mb-6">Nos Spécialités</h2>
                     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
                         <article className="shadow-md rounded-lg p-4 bg-gray-50">
-                            <Image src="/images/burger-special.jpg" alt="Burger Spécial" width={400} height={300} className="rounded-lg" />
+                            <Image src="https://yvt0xuhvxht9er2d.public.blob.vercel-storage.com/images/burger.svg" alt="Burger Spécial" width={400} height={300} className="rounded-lg" />
                             <h3 className="mt-4 text-xl font-bold text-teal-900">Le Giga Burger</h3>
                             <p className="text-gray-600 mt-2">Un mélange parfait de saveurs avec notre viande 100% locale et des ingrédients frais.</p>
                         </article>
                         <article className="shadow-md rounded-lg p-4 bg-gray-50">
-                            <Image src="/images/frites-special.jpg" alt="Frites Maison" width={400} height={300} className="rounded-lg" />
+                            <Image src="https://yvt0xuhvxht9er2d.public.blob.vercel-storage.com/images/frite.png" alt="Frites Maison" width={400} height={300} className="rounded-lg" />
                             <h3 className="mt-4 text-xl font-bold text-teal-900">Frites Maison</h3>
                             <p className="text-gray-600 mt-2">Des frites croustillantes préparées avec soin pour accompagner votre repas.</p>
                         </article>
                         <article className="shadow-md rounded-lg p-4 bg-gray-50">
-                            <Image src="/images/dessert-special.jpg" alt="Desserts Gourmands" width={400} height={300} className="rounded-lg" />
+                            <Image src="https://yvt0xuhvxht9er2d.public.blob.vercel-storage.com/images/dessert.svg" alt="Desserts Gourmands" width={400} height={300} className="rounded-lg" />
                             <h3 className="mt-4 text-xl font-bold text-teal-900">Desserts Gourmands</h3>
                             <p className="text-gray-600 mt-2">Terminez votre repas avec nos desserts maison, un vrai délice.</p>
                         </article>
